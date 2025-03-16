e092da179ece2539e63596d31fbb30f37c039211
Thom <thomvanrijn@gmail.com>
2025-03-16 23:09:44 +0700

    Finished version of rps game with working ui

--- js-script.js
+++ js-script.js
@@ -6,13 +6,13 @@ const btnPaper = document.getElementById("btn-paper");
 const btnScissors = document.getElementById("btn-scissors");
 const para = document.getElementById("para");
 const scoreContainer = document.getElementById("score-container");
-const humanScoreCounter = document.getElementById("human-score-counter");
+const playerScoreCounter = document.getElementById("player-score-counter");
 const computerScoreCounter = document.getElementById("computer-score-counter");
 const buttonContainer = document.getElementById("btn-container")
 const buttons = document.querySelectorAll("button")
 
 
-let humanScore = 0;
+let playerScore = 0;
 let computerScore = 0;
 
 
@@ -43,72 +43,99 @@ function getComputerInput() {
     };
 }
 
+function gameOver(playerScore, computerScore) {
+    if (playerScore == 5 || computerScore == 5){
+        para.textContent = (playerScore == 5) ? "Game over, you won!" : "Game over, you lost"
+        
+        btnPaper.style.visibility = "hidden"
+        btnRock.textContent = "Play again"
+        btnScissors.textContent = "Quit"
+        btnRock.addEventListener("click", newGame, {once: true})
+        btnScissors.addEventListener("click", () => window.close())
 
 
+        
+           
+        
+    }
+    
+        
+
+    
+    
+}
 
 
-
-btnPaper.innerText = "Start new game"
-btnPaper.addEventListener("click", () =>{
+function newGame() {
     btnPaper.innerText = "Paper";
     btnScissors.style.visibility = "visible";
     btnRock.style.visibility = "visible";
     para.innerText = "Pick: Rock, Paper or Scissors";
     scoreContainer.style.visibility = "visible"
-   
+    playerScore = 0;
+    computerScore = 0;
+    updateScore()
     
-    }, {once: true})
 
-const updateScore = () => {
-    humanScoreCounter.textContent = humanScore;
+}
+
+
+
+
+btnPaper.innerText = "Start new game"
+btnPaper.addEventListener("click", newGame, {once: true})
+
+function updateScore() {
+    playerScoreCounter.textContent = playerScore;
     computerScoreCounter.textContent = computerScore;
 }
 
-const playRound = (playerInput, computerInput) => {
-
-    while (humanScore > 5 || computerScore > 5 ) {
-        console.log("WOW")
-        if (!(playerInput === computerInput)){
-            switch (playerInput) {
-                case "rock":
-                    if(computerInput == "scissors"){
-                        playerScore ++;
-                        para.innerText = "You played ROCK, your opponent played SCISSORS. You WIN!";
-                    }
-                    else{
-                        computerScore ++;
-                        para.innerText = "You played ROCK, your opponent played PAPER. You LOSE!";
-                    }
-                    break
-                case "paper":
-                    if(computerInput == "rock"){
-                        playerScore ++;
-                        para.innerText = "You played PAPER, your opponent played ROCK. You WIN!";
-                    }
-                    else{
-                        computerScore ++;
-                        para.innerText = "You played PAPER, your opponent played SCISSORS. You LOSE!";
-                    }
-                    break
-                case "scissors":
-                    if(computerInput == "paper"){
-                        playerScore ++;
-                        para.innerText = "You played SCISSORS, your opponent played PAPER. You WIN!";
-                    }
-                    else{
-                        computerScore ++;
-                        para.innerText = "You played SCISSORS, your opponent played ROCK. You LOSE!";
-                    }
-                    break
-         }
-            
-        
-        }
-        else{
-            para.innerText = "You both picked " + playerInput + " choose again"
-
+function playRound(playerInput, computerInput) {
+      
+    
+    if (!(playerInput === computerInput)) {
+        switch (playerInput) {
+            case "rock":
+                if (computerInput == "scissors") {
+                    playerScore++;
+                    para.innerText = "You played ROCK, your opponent played SCISSORS. You WIN!";
+                }
+                else {
+                    computerScore++;
+                    para.innerText = "You played ROCK, your opponent played PAPER. You LOSE!";
+                }
+                break;
+            case "paper":
+                if (computerInput == "rock") {
+                    playerScore++;
+                    para.innerText = "You played PAPER, your opponent played ROCK. You WIN!";
+                }
+                else {
+                    computerScore++;
+                    para.innerText = "You played PAPER, your opponent played SCISSORS. You LOSE!";
+                }
+                break;
+            case "scissors":
+                if (computerInput == "paper") {
+                    playerScore++;
+                    para.innerText = "You played SCISSORS, your opponent played PAPER. You WIN!";
+                }
+                else {
+                    computerScore++;
+                    para.innerText = "You played SCISSORS, your opponent played ROCK. You LOSE!";
+                }
+                break;
         }
 
 
+    }
+    else {
+        para.innerText = "You both picked " + playerInput + ", choose again";
 
-}}
+    }
+updateScore();
+gameOver(playerScore, computerScore);
+
+
+
+}
